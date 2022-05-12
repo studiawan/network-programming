d323372b1931ea9006d151e4f9dc5a85211c7e17
Hudan Studiawan <studiawan@gmail.com>
2022-05-12 15:03:03 +0700

    Add bab09

---
+++ bab09/xmlrpc-boolean-client.py
@@ -0,0 +1,7 @@
+# https://docs.python.org/3/library/xmlrpc.client.html
+
+import xmlrpc.client
+
+with xmlrpc.client.ServerProxy("http://localhost:8000/") as proxy:
+    print("3 is even: %s" % str(proxy.is_even(3)))
+    print("100 is even: %s" % str(proxy.is_even(100)))
---
+++ bab09/xmlrpc-boolean-server.py
@@ -0,0 +1,11 @@
+# https://docs.python.org/3/library/xmlrpc.client.html
+
+from xmlrpc.server import SimpleXMLRPCServer
+
+def is_even(n):
+    return n % 2 == 0
+
+server = SimpleXMLRPCServer(("localhost", 8000))
+print("Listening on port 8000...")
+server.register_function(is_even, "is_even")
+server.serve_forever()
