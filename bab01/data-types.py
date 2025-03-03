10c3e4cb9b2bab9d2ee1e07fa72274f50b3c942c
Hudan Studiawan <studiawan@gmail.com>
2025-03-03 07:28:20 +0700

    Add data types and function examples

---
+++ bab01/data-types.py
@@ -0,0 +1,33 @@
+# Integer
+x = 10  
+print(type(x))  # Output: <class 'int'>
+
+# Float
+y = 3.14  
+print(type(y))  # Output: <class 'float'>
+
+s = "Hello, Python!"
+print(type(s))  # Output: <class 'str'>
+
+# String concatenation
+greeting = "Hello" + " " + "World"
+print(greeting)  # Output: Hello World
+
+# Accessing characters
+print(s[0])  # Output: H
+print(s[-1])  # Output: !
+
+# String slicing
+print(s[0:5])  # Output: Hello
+
+# String methods
+print(s.lower())  # Output: hello, python!
+print(s.upper())  # Output: HELLO, PYTHON!
+
+is_active = True
+is_logged_in = False
+
+print(type(is_active))  # Output: <class 'bool'>
+print(5 > 3)  # Output: True
+print(5 < 3)  # Output: False
+
